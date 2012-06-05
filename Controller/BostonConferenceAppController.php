b7dcf1c792164fdc3e8b50a1e8def632c9cf9991
andrewcurioso <andrew@andrewcurioso.com>
2012-06-05 00:48:02 -0400

    Move everything navigation menu related to a component

--- Controller/BostonConferenceAppController.php
+++ Controller/BostonConferenceAppController.php
@@ -20,11 +20,11 @@ class BostonConferenceAppController extends AppController {
 	public $helpers = array('BostonConference.ContentManagement');
 
 /**
- * navigationLinks
+ * components
  *
  * @var array
  */
-	private $navigationLinks = array();
+	public $components = array('BostonConference.Menu');
 
 /**
  * Called before the controller action.  You can use this method to configure and customize components
@@ -36,7 +36,6 @@ class BostonConferenceAppController extends AppController {
 	public function beforeFilter() {
 		parent::beforeFilter();
 
-		$this->_createDefaultNavigationLinks();
 		$this->_populateAuthData();
 
 		$this->set('is_admin_area',$this->params['admin']);
@@ -44,32 +43,6 @@ class BostonConferenceAppController extends AppController {
 		if ( $this->params['admin'] ) {
 			$this->set('skinny_sidebar',true);
 		}
-	}
-
-/**
- * Creates the default navigation links. Can be overriden in individule controllers.
- *
- * @returns void
- */
-	protected function _createDefaultNavigationLinks() {
-		$isAdmin = $this->params['admin'];
-
-		if ( $isAdmin ) {
-			$this->addNavigationLink('Home',array('plugin' => 'BostonConference', 'controller' => 'boston_conference', 'action' => 'index'));
-			$this->addNavigationLink('Events',array('plugin' => 'BostonConference', 'controller' => 'events', 'action' => 'index'));
-			$this->addNavigationLink('News',array('plugin' => 'BostonConference', 'controller' => 'news', 'action' => 'index'));
-		} else {
-			$this->addNavigationLink('Home',array('plugin' => 'BostonConference', 'controller' => 'news', 'action' => 'index'));
-		}
-
-		$this->addNavigationLink('Schedule',array('plugin' => 'BostonConference', 'controller' => 'talks', 'action' => 'index'));
-
-		$this->addNavigationLink('Sponsors',array('plugin' => 'BostonConference', 'controller' => 'sponsors', 'action' => 'index'));
-
-		$this->addNavigationLink('Venue'.($isAdmin ? 's' : ''),array('plugin' => 'BostonConference', 'controller' => 'venues', 'action' => 'index'));
-
-		if ( $isAdmin )
-			$this->addNavigationLink('Hotels',array('plugin' => 'BostonConference', 'controller' => 'hotels', 'action' => 'index'));
 	}
 
 /**
@@ -96,28 +69,15 @@ class BostonConferenceAppController extends AppController {
 	}
 
 /**
- * Adds a link to the top navigation.
- *
- * @param string $label The label for the link.
- * @param mixed $link The actual link. Can be a path or link data structure.
- * @returns void
- */
-	public function addNavigationLink( $label, $link ) {
-		$this->navigationLinks[] = array( $label, $link );
-	}
-
-/**
  * Called after the controller action is run, but before the view is rendered. You can use this method
  * to perform logic or set view variables that are required on every request.
- * Sets the navigation links for the view.
  *
  * @return void
  */
 	public function beforeRender() {
-		$this->set('navigation_links',$this->navigationLinks);
 		$this->set('element_path', array( preg_replace('/Controller$/','',get_class($this)), $this->action ));
 
-		return parent::beforeRender();
+		parent::beforeRender();
 	}
 
 }
---
+++ Controller/Component/MenuComponent.php
@@ -0,0 +1,173 @@
+<?php
+App::uses('Component', 'Controller');
+
+/**
+ * MenuComponent
+ *
+ */
+class MenuComponent extends Component {
+
+/**
+ * navigationLinks
+ *
+ * @var array
+ */
+	protected $_navigationLinks = array();
+
+/**
+ * Initializes the component.
+ *
+ * @param Controller The controller being used
+ * @returns void
+ */
+	public function initialize( Controller $controller ) {
+		$this->_initDefaultMenu($controller);
+	}
+
+/**
+ * Adds a link to the menu.
+ *
+ * @param string $label The label for the link.
+ * @param mixed $link The actual link. Can be a path or link data structure.
+ * @returns void
+ */
+	public function addLink( $label, $link, $importance=100 ) {
+		$this->_navigationLinks[] = array( $label, $link, $importance );
+	}
+
+/**
+ * Called after the controller action is run, but before the view is rendered. You can use this method
+ * to perform logic or set view variables that are required on every request.
+ * Sets the navigation links for the view.
+ *
+ * @param Controller $controller The controller using this component
+ * @return void
+ */
+	public function beforeRender( Controller $controller ) {
+
+		usort($this->_navigationLinks,array($this,'_sortCallback'));
+
+		$controller->set('navigation_links',$this->_navigationLinks);
+
+		parent::beforeRender($controller);
+	}
+
+/**
+ * Sorts the menu items based on importance.
+ *
+ * @param array $a The first item to compair
+ * @param array $b The second item to compair
+ * @returns int 0 if $a and $b are equal, -1 if $a < $b and 1 if $a > $b
+ */
+	protected function _sortCallback($a,$b) {
+		if ( $a[2] == $b[2] ) return 0;
+		return ( $a[2] < $b[2] ? -1 : 1 );
+	}
+
+/**
+ * Creates the default navigation links.
+ *
+ * @param Controller $controller The controller being used.
+ * @returns void
+ */
+	protected function _initDefaultMenu(Controller $controller) {
+		$isAdmin = $controller->params['admin'];
+
+		if ( $isAdmin ) {
+
+			// Home link (0 - most important)
+			$this->addLink(
+				'Home',
+				array(
+					'plugin' => 'BostonConference',
+					'controller' => 'boston_conference',
+					'action' => 'index'
+				),
+				0
+			);
+
+			// Events admin link (10)
+			$this->addLink(
+				'Events',
+				array(
+					'plugin' => 'BostonConference',
+					'controller' => 'events',
+					'action' => 'index'
+				),
+				10
+			);
+
+			// News admin link (20)
+			$this->addLink(
+				'News',
+				array(
+					'plugin' => 'BostonConference',
+					'controller' => 'news',
+					'action' => 'index'
+				),
+				20
+			);
+
+		} else {
+			// Home link (0 - most important)
+			$this->addLink(
+				'Home',
+				array(
+					'plugin' => 'BostonConference',
+					'controller' => 'news',
+					'action' => 'index'
+				),
+				0
+			);
+		}
+
+		// Schedule link (30)
+		$this->addLink(
+			'Schedule',
+			array(
+				'plugin' => 'BostonConference',
+				'controller' => 'talks',
+				'action' => 'index'
+			),
+			30
+		);
+
+		// Sponsors link (40)
+		$this->addLink(
+			'Sponsors',
+			array(
+				'plugin' => 'BostonConference',
+				'controller' => 'sponsors',
+				'action' => 'index'
+			),
+			40
+		);
+
+		// Vanue link (50)
+		$this->addLink(
+			'Venue'.($isAdmin ? 's' : ''),
+			array(
+				'plugin' => 'BostonConference',
+				'controller' => 'venues',
+				'action' => 'index'
+			),
+			50
+		);
+
+		if ( $isAdmin ) {
+
+			// Admin hotels link (60)
+			$this->addLink(
+				'Hotels',
+				array(
+					'plugin' => 'BostonConference',
+					'controller' => 'hotels',
+					'action' => 'index'
+				),
+				60
+			);
+
+		}
+	}
+
+}
